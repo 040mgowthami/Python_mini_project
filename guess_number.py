13310367561a5c9751a2d95b8b5545bc5cfdd0fa
Gowthami M <168198692+040mgowthami@users.noreply.github.com>
2026-07-12 22:45:24 +0530

    Create guess_number program

---
+++ guess_number.py
@@ -0,0 +1,8 @@
+import random
+number=random.random.randint(1,10)
+guees=int(input("guees the number between 1 to 10")
+if guees==number:
+          print("correct")
+else:
+  print("wrong")
+  print("correct number was:",number)
