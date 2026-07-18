0b5fcbca1f7aeef4a3d822e3b52fff135a279236
Gowthami M <168198692+040mgowthami@users.noreply.github.com>
2026-07-18 20:14:37 +0530

    Create simple_bill program

---
+++ simple_bill.py
@@ -0,0 +1,7 @@
+item1 = float(input("Enter Item 1 Price: "))
+item2 = float(input("Enter Item 2 Price: "))
+item3 = float(input("Enter Item 3 Price: "))
+
+total = item1 + item2 + item3
+
+print("Total Bill =", total)
